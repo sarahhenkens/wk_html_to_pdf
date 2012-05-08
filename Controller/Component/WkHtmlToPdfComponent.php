789b88b5b7b3003bc69a3033a128979636c38bdf
Jelle Henkens <jellehenkens@geneo.co.uk>
2012-05-08 16:56:58 +0100

    Updating classes to 2.x

--- Controller/Component/WkHtmlToPdfComponent.php
+++ Controller/Component/WkHtmlToPdfComponent.php
@@ -1,21 +1,25 @@
 <?php
-	class WkHtmlToPdfComponent extends Object {
-		
-		/**
-		 * @brief initialise the WkHtmlToPdf Component
-		 * 
-		 * If there is an extention "pdf" in the url automatically set WkHtmlToPdf 
-		 * as the View class
-		 * 
-		 * @access public
-		 * 
-		 * @param type $controller 
-		 * 
-		 * @return void
-		 */
-		public function initialize(&$controller) {
-			if ($controller->RequestHandler->ext == 'pdf') {
-				$controller->view = 'WkHtmlToPdf.WkHtmlToPdf';
-			}
+
+
+App::uses('Component', 'Controller');
+
+class WkHtmlToPdfComponent extends Component {
+
+	/**
+	 * @brief initialise the WkHtmlToPdf Component
+	 * 
+	 * If there is an extention "pdf" in the url automatically set WkHtmlToPdf 
+	 * as the View class
+	 * 
+	 * @access public
+	 * 
+	 * @param type $controller 
+	 * 
+	 * @return void
+	 */
+	public function startup(Controller $controller) {
+		if(isset($controller->request->params['ext']) && $controller->request->params['ext'] == 'pdf') {
+			$controller->viewClass = 'WkHtmlToPdf.WkHtmlToPdf';
 		}
 	}
+}
